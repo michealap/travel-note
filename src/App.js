bc23cfb4bde8d6adac94aaf417b0ad0e97193cb4
Noya Onggud <yanuo44@gmail.com>
2022-02-11 13:44:00 -0500

    add searchbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import Note from "./components/Note";
+import Search from "./components/Search";
 import { render } from "@testing-library/react";
 
 function App() {
-  return <Note />;
+  return (
+    <div>
+      <Search />
+      <hr />
+      <Note />
+    </div>
+  );
 }
 
 export default App;
