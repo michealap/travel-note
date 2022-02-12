1d1f6400f2b385a74c3543cd5acd0717af836474
Matthew <matthew@Matthews-MBP.phub.net.cable.rogers.com>
2022-02-12 16:51:34 -0500

    axios call for weather

--- src/helpers/axioscall.js
+++ src/helpers/axioscall.js
@@ -2,6 +2,10 @@ const axios = require("axios");
 
 export default function axioscall(countryName) {
   const ninja_api_key = process.env.REACT_APP_NINJA_API_KEY;
+  //const weather_api_key = process.env.REACT_APP_WEATHER_API_KEY;
+  
+
+
   function standardize(input) {
     // console.log("here", input);
     let i = input.trim().toLowerCase();
@@ -21,8 +25,10 @@ export default function axioscall(countryName) {
     }
     return result;
   }
+
   const validatedUrl = validUrl(countryName);
   const api_ninja_country = `https://api.api-ninjas.com/v1/country?name=${validatedUrl}`;
+  //const api_country_flag = `https://countryflagsapi.com/:png/${countryName}`;
 
     return axios
       .get(api_ninja_country, {
@@ -33,4 +39,19 @@ export default function axioscall(countryName) {
       .then((res) => {
         console.log(res.data[0]);
       });
+    
+    axios
+    .get(api_ninja_country, {
+      headers: {
+        "X-Api-Key": ninja_api_key,
+      },
+    })
+    .then((res) => {
+      return axios.get(`http://api.weatherapi.com/v1/current.json?key=${weather_api_key}&q=${res.data[0].capital}`)
+    })
+    .then((res) => {
+      console.log(res.data[0]);
+    });
+
+    
 }
