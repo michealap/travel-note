25a73302c4905377e74bf49f299a7904d42f5091
Matthew <matthew@Matthews-MBP.phub.net.cable.rogers.com>
2022-02-13 16:49:57 -0500

    Weather api working

--- src/helpers/axioscall.js
+++ src/helpers/axioscall.js
@@ -3,13 +3,11 @@ const axios = require("axios");
 export default function axioscall(countryName) {
   const ninja_api_key = process.env.REACT_APP_NINJA_API_KEY;
   const weather_api_key = process.env.REACT_APP_WEATHER_KEY;
-  console.log("Weather api key", weather_api_key);
-
+  //console.log("Weather api key", weather_api_key);
   let countryStats = [];
+  let city;
   function standardize(input) {
-    // console.log("here", input);
     let i = input.trim().toLowerCase();
-    // console.log("here", i);
     return i;
   }
 
@@ -37,21 +35,25 @@ export default function axioscall(countryName) {
   const validatedUrl = validUrl(countryName);
   const api_ninja_country = `https://api.api-ninjas.com/v1/country?name=${validatedUrl}`;
   
-
-    /* axios
+    //WORKING country api-call
+     axios
       .get(api_ninja_country, {
         headers: {
           "X-Api-Key": ninja_api_key,
         },
       })
       .then((res) => {
-        console.log(res.data[0]);
+        
         countryStats = res.data[0];
-        console.log("Country Stats", countryStats)
+        city = res.data[0].capital;
+        //console.log("capital", res.data[0].capital);
+        //console.log("Country Stats", countryStats)
       }).catch(err => {
         console.log(err);
-      }) */
+      }) 
+    
 
+    //WORKING weather api-call
     axios
       .get(api_ninja_country, {
         headers: {
@@ -62,15 +64,17 @@ export default function axioscall(countryName) {
         //console.log("ninja data", res.data[0].capital);
         //console.log("weather api key", weather_api_key);
         let city = validCapital(res.data[0].capital);
-        //console.log("City", city);
+        console.log("City", city);
         return axios.get(`http://api.weatherapi.com/v1/current.json?key=${weather_api_key}&q=${city}`)
         .then((res) => {
-          console.log("weather data", res.data[0]);
+          console.log("weather data", res.data.location);
         })
         .catch((err) => {
           console.log(err);
         })
       })
+
+    
       
 
      // WORKS: http://api.weatherapi.com/v1/current.json?key=c353a72ace5742e490d191454221202&q=$Ottawa
