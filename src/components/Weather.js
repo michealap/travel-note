03bb5ed48a54a517ade11d61de7919e8c6513957
Matthew <matthew@Matthews-MBP.phub.net.cable.rogers.com>
2022-02-14 15:40:27 -0500

    Icon/condition for weather

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,23 +4,19 @@ export default function Weather({ weatherStats, countryStats }) {
   if(!weatherStats) { //starting state is false
     return <></>;
   }
-
-
-  //console.log("Weather stat", weatherStats);
-  //console.log("weather condition", weatherStats.condition);
+  console.log("weather stats", weatherStats);
   let weatherCondition = weatherStats.condition;
-  //console.log("weather condition", weatherStats);
   
   return (
     <div>
-      <h2>{countryStats.name} Capital - {countryStats.capital}'s' weather</h2>
+      <h2>{countryStats.name} Capital - {countryStats.capital}'s weather</h2>
       {/*<p>Condition {weatherStats.condition}</p>  */}
-      <p>It's {weatherCondition.text} outside </p>
-      <p>Temperature {weatherStats.temp_c} C / {weatherStats.temp_f} F</p>
+      <p>It's {weatherCondition.text} outside today</p><img src={weatherCondition.icon} />
+      <p>The temperature in {countryStats.capital} is {weatherStats.temp_c} C / {weatherStats.temp_f} F</p>
       <p>But it feels more like {weatherStats.feelslike_c} C / {weatherStats.feelslike_f} F with the wind.</p>
       
-      <p>The humidity {weatherStats.humidity}</p> 
-      
+      <p>The humidity is {weatherStats.humidity}</p> 
+      {weatherStats.precip_mm > 0 ? <p>It's going to rain today</p> : <p>It's not going to rain today</p>}
     </div>
   )
 }
