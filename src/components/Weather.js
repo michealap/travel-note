54b955702694da87937c3b66840736cf3750a5a3
Micheala Palmer <michealapalmer@gmail.com>
2022-02-14 11:49:03 -0500

    cleanup code

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,7 +7,8 @@ export default function Weather({ weatherStats }) {
   return (
     <div>
       <h2>Current Weather</h2>
-      {/* <p>{weatherStats.condition.text}</p> this one doesnt allow page to load similar to currency code*/}
+      {/* <p>Condition {weatherStats.condition.text}</p>  */}
+      {/* this one doesnt allow page to load similar to currency code */}
       <p>Temperature {weatherStats.temp_c} C / {weatherStats.temp_f} F</p>
       <p>Humidity {weatherStats.humidity}</p>
     </div>
