f437dcc79c0c4f143cdf8c21d15defc7aaa60c9d
Matthew <matthew@Matthews-MBP.phub.net.cable.rogers.com>
2022-02-16 10:09:11 -0500

    Currency axios call

--- src/helpers/axioscall.js
+++ src/helpers/axioscall.js
@@ -50,6 +50,7 @@ export default async function axioscall(countryName) {
   const api_ninja_country = `https://api.api-ninjas.com/v1/country?name=${validatedUrl}`;
   const youtube_api_call = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=date&q=${validatedUrl}+vacation&key=${youtube_api_key}`;
   
+  
     //WORKING country api-call
     const res = await axios
       .get(api_ninja_country, {
@@ -60,6 +61,7 @@ export default async function axioscall(countryName) {
     
     countryStats = res.data[0];
     let city = validCapital(countryStats.capital);
+    
 
     //WORKING weather api-call
     const weatherRes = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${weather_api_key}&q=${city}`)
@@ -83,9 +85,30 @@ export default async function axioscall(countryName) {
       newsList = res.data.data;
       //console.log("Final list", newsList);
     });
-  
+
+    let exchangeCurrency;
+    
+    if(countryStats) {
+      exchangeCurrency = countryStats.currency.code;
+    }
+    const currency_api_call = `https://api.api-ninjas.com/v1/convertcurrency?have=CAD&want=${exchangeCurrency}&amount=1`;
+    
+
+    let currencyConvert = await axios
+      .get(currency_api_call, {
+        headers: {
+          "X-Api-Key": ninja_api_key,
+        }
+    })
+
+    if(currencyConvert) {
+      currencyConvert = currencyConvert.data;
+    }
+      
+    
+      console.log("Currency convert", currencyConvert);
       console.log("country stats", countryStats);
       console.log("Weather stats", weatherStats);
       console.log('Video List', videos);
-    return {countryStats, weatherStats, videos, newsList};  
+    return {countryStats, weatherStats, videos, newsList, currencyConvert};  
 }
