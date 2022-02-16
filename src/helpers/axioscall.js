b27e9c2dacf54747900b97f93fe8c9ca9ec5efff
Matthew <matthew@Matthews-MBP.phub.net.cable.rogers.com>
2022-02-15 19:53:13 -0500

    news call

--- src/helpers/axioscall.js
+++ src/helpers/axioscall.js
@@ -8,12 +8,12 @@ export default async function axioscall(countryName) {
   const ninja_api_key = process.env.REACT_APP_NINJA_API_KEY;
   const weather_api_key = process.env.REACT_APP_WEATHER_KEY;
   const youtube_api_key = process.env.REACT_APP_YOUTUBE_API_KEY;
-  //const news_api_key = process.env.REACT_APP_NEWS_API_KEY;
+  const news_api_key = process.env.REACT_APP_NEWS_API_KEY;
   
   let countryStats = {};
   let weatherStats = {};
   let videoList = {};
-  //let newsList = {};
+  let newsList = {};
   
 
   function standardize(input) {
@@ -49,7 +49,7 @@ export default async function axioscall(countryName) {
   const validatedUrl = validUrl(countryName);
   const api_ninja_country = `https://api.api-ninjas.com/v1/country?name=${validatedUrl}`;
   const youtube_api_call = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=date&q=${validatedUrl}%2bvacation&key=${youtube_api_key}`;
-  //const news_api_call = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/TrendingNewsAPI';
+  //const news_api_call = 'http://api.mediastack.com/v1/news';
   
     //WORKING country api-call
     const res = await axios
@@ -73,6 +73,37 @@ export default async function axioscall(countryName) {
     videoList = youtubeRes.data;
     
     //News api-call
+
+    const params = {
+      access_key: news_api_key
+    };
+
+    const newsRes = axios.get('http://api.mediastack.com/v1/news', {params})
+    /* .then(response => {
+        console.log("news response", response)
+        newsList = response.data;
+    }).catch(error => {
+        console.log(error);
+    }); */
+    newsRes.then((res) => {
+      newsList = res.data.data;
+      //console.log("Final list", newsList);
+    });
+    
+    //console.log("News response", newsRes);
+
+
+
+    /* const newsRes = await axios
+      .get(news_api_call)
+      .then((res) => {
+        console.log("news call response", res);
+      })
+      .catch((err) => {
+        console.log("Error code", err);
+      })
+
+    newsList = newsRes; */
     /* var options = {
       method: 'GET',
       url: news_api_call,
@@ -94,10 +125,13 @@ export default async function axioscall(countryName) {
     console.log("country stats", countryStats);
     console.log("Weather stats", weatherStats);
       
-    return {countryStats, weatherStats, videoList };  
+    return {countryStats, weatherStats, videoList, newsList };  
 }
 
 //YOUTUBE Call WORKS - regular validation works 
 //https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=date&q=united+states+vacation&key=
+
+//News call 
+
 
 
