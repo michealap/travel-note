3a35b7a26924671c644a1fe8f67758c0b2772f01
Matthew <matthew@Matthews-MBP.phub.net.cable.rogers.com>
2022-02-18 19:57:25 -0500

    Built with section

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -7,13 +7,22 @@ function About() {
     <NavBar />
     <h3 className="about-title">About Us</h3>
     <div className="about-paragraph">
-      <p>This the where we talk about ourselves</p>
-      <p>This is where Micheala will talk about herself</p>
-      <p>This is where Noya will talk about himself</p>
-      <p>This is where Matthew will talk about himself</p>
+      <p>Here are some of the members of our talented team:</p>
+      <p>This is where Micheala will talk about herself + empty image</p>
+      <p>This is where Noya will talk about himself + empty image</p>
+      <p>This is where Matthew will talk about himself + empty image</p>
     </div>
+    <p className="about-tools-title">Built using</p>
     <div className="about-tools">
-      <p>This is the tools section</p>
+      <a href="https://reactjs.org/">
+      <img src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png" alt="react" width="100px" height="100px" className="react-class"></img>
+      </a>
+      <a href="https://supabase.com/">
+      <img src="https://pbs.twimg.com/profile_images/1397471927132844033/jN-wuufb_400x400.jpg" alt="supabase" width="100px" height="100px" className="react-class"></img>
+      </a>
+      <a href="https://api-ninjas.com/">
+      <img src="https://rapidapi.com/cdn/images?url=https://rapidapi-prod-apis.s3.amazonaws.com/4784b7c0-b6e5-4ffb-b7b3-50f41c99a0fa.png" alt="ninja-api" width="100px" height="100px"></img>
+      </a>
     </div>
   </div>;
 }
