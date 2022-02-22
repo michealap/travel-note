eb871db7a1e7f44aaabf4846132801ff93e83d2b
Micheala Palmer <michealapalmer@gmail.com>
2022-02-22 16:35:09 -0500

    add styles and layout

--- src/components/Pages/Logout.js
+++ src/components/Pages/Logout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import NavBar from "../NavBar";
+import Typography from "@mui/material/Typography";
 
 function Logout() {
   return (
     <div>
       <NavBar />
-      You've succefully logged out.See you next time!
+      <Typography variant="h5" pt={8} textAlign="center">You've succefully logged out. See you next time! <br></br>
+      <iframe src="https://giphy.com/embed/vBMzK6KN6M6zK" width="20%" height="25%" frameBorder="0" className="giphy-embed" allowFullScreen title="travel"></iframe>
+      </Typography>
     </div>
   );
 }
